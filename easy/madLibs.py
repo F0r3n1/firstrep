0d8e9f7b35b4914f07fab538a79c4d2d89aac5a5
Daniyar <f0r3n1@gmail.com>
2025-06-09 21:20:26 +0300

    DaD

---
+++ easy/madLibs.py
@@ -0,0 +1,6 @@
+from random import randint
+text1="Однажды {name} пошёл в {place}, чтобы {verb}. Это было очень {adjective}!"
+text2="Все знали, что {name} обожает {verb} в {place}. Хотя это звучит {adjective}, ему было всё равно."
+text3="{name} проснулся утром, почувствовал себя {adjective} и сразу решил отправиться в {place}, чтобы {verb}."
+ 
+
