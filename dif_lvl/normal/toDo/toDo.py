74c5b62e57d1538d930a7264cb8d5974da884fb7
For3n <F0r3n1@gmail.com>
2025-08-04 00:33:26 +0300

    коммит между работой

--- dif_lvl/normal/toDo/toDo.py
+++ dif_lvl/normal/toDo/toDo.py
@@ -1,12 +1,12 @@
 import json
 
-with open(r"C:\Users\f0r3n\Desktop\ваэно\pyproject\firstrep\dif_lvl\normal\toDo\toDo.json", "r") as file:
+with open(r"C:\Users\f0r3n\Desktop\ваэно\pyproject\firstrep\dif_lvl\normal\toDo\toDo.json", "r",encoding="utf-8") as file:
     toDo_listR = json.load(file)
 
-def save():
-    with open("toDo.json", "w", encoding="utf-8") as file:
-        json.dump(tasks, file, ensure_ascii=False, indent=4)
 
+def save():
+        with open("C:\\Users\\f0r3n\\Desktop\\ваэно\\pyproject\\firstrep\\dif_lvl\\normal\\toDo\\toDo.json", "w", encoding="utf-8") as file:
+            json.dump(toDo_listR or toDo_list, file, ensure_ascii=False, indent=4)
 
 while True:
     choice=input("Select an action: add / show / delete / exit: ").lower()
@@ -20,14 +20,15 @@ while True:
     
     
     
-    if choice=="add":
-        toDo_listW=dict
+    with open("C:\\Users\\f0r3n\\Desktop\\ваэно\\pyproject\\firstrep\\dif_lvl\\normal\\toDo\\toDo.json", "r", encoding="utf-8") as file:
+        toDo_list = json.load(file)
+
+    if choice == "add":
         new_key = input("Введите название задачи (например, 'Покушать'): ")
         new_value = input("Опиши подробнее (например, 'Съесть пиццу'): ")
 
-        toDo_listW[new_key] = new_value
-        with open("C:\\Users\\f0r3n\\Desktop\\ваэно\\pyproject\\firstrep\\dif_lvl\\normal\\toDo\\toDo.json","w",ensure_ascii=False) as file:
-            json.dump(toDo_listW,file)
+        toDo_list[new_key] = new_value
+        save()
 
     
     elif choice=="show":
@@ -35,11 +36,15 @@ while True:
     
     elif choice == "delete":
         show()
-        toDelete = int(input("Выберете НОМЕР объекта, который хотите удалить: ")) - 1
-        if input(f"Вы уверены что хотите удалить объект {toDo_list[toDelete]}? (yes/no): ").lower() == "yes":
-            del toDo_list[toDelete]
-            
-        
+        key_to_delete = input("Введите точное название задачи, которую хотите удалить: ")
+
+
+        if key_to_delete in toDo_listR:
+            del toDo_listR[key_to_delete]
+            save()     
+            print(f"Задача '{key_to_delete}' удалена.")
+        else:
+            print("Такой задачи нет.")
         
     
     else:
@@ -49,7 +54,3 @@ while True:
 
 
 
-
-with open("C:\\Users\\f0r3n\\Desktop\\ваэно\\pyproject\\firstrep\\dif_lvl\\normal\\toDo\\toDo.json","w") as file:
-    json.dump(toDo_list,file)
-    print(toDo_list)
